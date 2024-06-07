d53b8ea51e51d7497cd98259db1f30e11bf1e307
kir565 <kir563web@gmail.com>
2024-06-07 11:21:47 +0700

    using native css

--- global.d.ts
+++ global.d.ts
@@ -9,7 +9,7 @@
 // declare module '*.module.css';
 // declare module '*.module.scss';
 
-declare module '*.module.scss' {
+declare module '*.module.css' {
   const content: Record<string, string>;
   export = content;
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 
 import { First } from 'components/first/First';
-import './index.scss';
+import './index.css';
 
 export const App: FC = () => {
   return (
